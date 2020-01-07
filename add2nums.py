203de8149852e798495bb7cd45ac86e7b9de6491
jessicagamio <jessica.gamio@gmail.com>
2020-01-06 21:46:45 -0800

    created linked list class to locate tail of linked list

--- add2nums.py
+++ add2nums.py
@@ -3,13 +3,19 @@ class Node(object):
         self.data = data
         self.next = None
 
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
 def add2num(ll1,ll2):
     curr1 = ll1
     curr2 = ll2
+    curr3 = None
 
     carryover=0
 
-    ll3 = None
+    ll3 = LinkedList()
 
     while curr1 or curr2:
 
@@ -20,21 +26,32 @@ def add2num(ll1,ll2):
             curr2.data = 0
 
         if curr1 and curr2:
+            print('curr1 ===',curr1.data)
+            print('curr2 ===', curr2.data)
             node = curr1.data + curr2.data + carryover
 
             if node >= 10:
-                carryover = node%10
-                node = abs(node/10)
+                carryover = node//10
+                print('carry over =', carryover)
+                node = node%10
+                print('node over 10 =', node)
 
 
-        if ll3 == None:
-            ll3 = Node(node)
+        if ll3.head == None:
+            ll3.head = Node(node)
+            print('ll3 head',ll3.head.data)
+            
+        elif ll3.head.next == None:
+            ll3.head.next = Node(node)
+            ll3.tail = ll3.head.next
+
         else:
-            ll3.next = Node(node)
-            ll3 = ll3.next
+            ll3.tail.next = Node(node)
+            ll3.tail = ll3.tail.next
 
-        print(ll3.data)
+            print('ll3 tail',ll3.tail.data)
 
+        print('-------------------------')
         curr1 = curr1.next
         curr2 = curr2.next
 
@@ -49,4 +66,5 @@ ll2.next = Node(6)
 ll2.next.next = Node(4)
 
 answer = add2num(ll1,ll2)
-print(answer)
+
+print('ll3 node =', answer.head.data, answer.head.next.data, answer.tail.data)
