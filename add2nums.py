98768444c21f86a669bfcf3154fb810041328b35
jessicagamio <jessica.gamio@gmail.com>
2020-01-06 21:52:24 -0800

    add comments to python code

--- add2nums.py
+++ add2nums.py
@@ -1,17 +1,19 @@
 class Node(object):
     def __init__(self,data):
+        """create node"""
         self.data = data
         self.next = None
 
 class LinkedList(object):
+    """create Linked List"""
     def __init__(self):
         self.head = None
         self.tail = None
 
 def add2num(ll1,ll2):
+    """add nodes in linked lists and return sum of each node"""
     curr1 = ll1
     curr2 = ll2
-    curr3 = None
 
     carryover=0
 
@@ -26,32 +28,27 @@ def add2num(ll1,ll2):
             curr2.data = 0
 
         if curr1 and curr2:
-            print('curr1 ===',curr1.data)
-            print('curr2 ===', curr2.data)
             node = curr1.data + curr2.data + carryover
 
+            # if node has a value in tens place carryover number to following node
             if node >= 10:
                 carryover = node//10
-                print('carry over =', carryover)
                 node = node%10
-                print('node over 10 =', node)
 
-
+        # if linked list has no node create head node
         if ll3.head == None:
             ll3.head = Node(node)
-            print('ll3 head',ll3.head.data)
             
+        # if no node follows head of linked list add tail
         elif ll3.head.next == None:
             ll3.head.next = Node(node)
             ll3.tail = ll3.head.next
 
+        # if no node exists after tail add new node to tail.next
         else:
             ll3.tail.next = Node(node)
             ll3.tail = ll3.tail.next
 
-            print('ll3 tail',ll3.tail.data)
-
-        print('-------------------------')
         curr1 = curr1.next
         curr2 = curr2.next
 
@@ -66,5 +63,3 @@ ll2.next = Node(6)
 ll2.next.next = Node(4)
 
 answer = add2num(ll1,ll2)
-
-print('ll3 node =', answer.head.data, answer.head.next.data, answer.tail.data)
